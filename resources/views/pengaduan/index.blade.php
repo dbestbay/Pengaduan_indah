c3d825f0f60e582a86a3cc42786ba024456b17af
Bayu Prayoga <106808389+dbestbay@users.noreply.github.com>
2023-02-28 11:43:10 +0700

    Update index.blade.php

--- resources/views/pengaduan/index.blade.php
+++ resources/views/pengaduan/index.blade.php
@@ -4,7 +4,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-12">
-                    <h1 class="m-0"><center><font color="A8A4CE">Pengaduan</font></center></h1>
+                    <h1 class="m-0"><center><font color="blue">Pengaduan</font></center></h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
